a5e28acf9d6f0a8508c9a1477be884597b6ed269
Gaurav1517 <145908403+Gaurav1517@users.noreply.github.com>
2024-11-25 22:33:51 +0530

    Update AppApplication.java

--- app/src/main/java/com/sample/app/AppApplication.java
+++ app/src/main/java/com/sample/app/AppApplication.java
@@ -1,14 +1,30 @@
+// package com.sample.app;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class AppApplication {
+
+//     public static void main(String[] args) {
+//         System.out.println("Hello"); // Print "Hello" to the console
+//         SpringApplication.run(AppApplication.class, args);
+//     }
+// }
+
 package com.sample.app;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
-public class AppApplication {
+@Controller
+public class HomeController {
 
-    public static void main(String[] args) {
-        System.out.println("Hello"); // Print "Hello" to the console
-        SpringApplication.run(AppApplication.class, args);
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Welcome to the Home Page!";
     }
 }
 
