f801492809f2c5a9bb7eba683edc0bf7031ce314
Gaurav1517 <145908403+Gaurav1517@users.noreply.github.com>
2024-11-25 22:00:43 +0530

    Update AppApplication.java

--- app/src/main/java/com/sample/app/AppApplication.java
+++ app/src/main/java/com/sample/app/AppApplication.java
@@ -1,26 +1,15 @@
-// package com.sample.app;
+package com.sample.app;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-// @SpringBootApplication
-// public class AppApplication {
-
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(AppApplication.class, args);
-// 	}
-
-// }
-
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AppApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
 
     @Bean
     public CommandLineRunner run() {
@@ -29,5 +18,8 @@ public class AppApplication {
             // Add custom startup logic here
         };
     }
+
 }
+
+
 
